6de637d8f336b7cf4492d6bb4de65d8be8005565
James <jknight@lunenfeld.ca>
2018-11-20 13:45:04 -0500

    Update introduction

--- components/introduction/introduction.js
+++ components/introduction/introduction.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Introduction = () => (
   <article>
+    <p>This help is a work in progress.</p>
     <p>
 ProHits-viz is a suite of web tools designed principally to aid visualizing
 protein-protein interaction data although it can be used for any data set
@@ -12,7 +13,9 @@ variety of analysis and display options.
     <p>
 The navigation menu at left can be used to find the help you need. If you have
 a question not covered by this guide, feel free to contact us at
+      {' '}
       <a href="mailto:contact@prohits-viz.org?Subject=ProHits-viz%20help">contact@prohits-viz.org</a>
+.
     </p>
   </article>
 );
